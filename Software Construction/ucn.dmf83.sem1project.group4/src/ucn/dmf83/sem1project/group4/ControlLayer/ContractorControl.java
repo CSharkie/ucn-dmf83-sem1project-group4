3d3813c7a0798ae82030d6e6b3d9e9db27456027
Tacx <cryszone@gmail.com>
2012-12-18 13:09:52 +0100

    Updated ControlLayer
    TODO: Write Comments
    TODO: Expand the controller commands.

--- Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/ContractorControl.java
+++ Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/ContractorControl.java
@@ -1,7 +1,31 @@
 package ucn.dmf83.sem1project.group4.ControlLayer;
 
+import java.util.ArrayList;
+
 import ucn.dmf83.sem1project.group4.DomainLayer.*;
 
 public class ContractorControl {
+	
+	private ContractorContainer contractor;
+	
+	public ContractorControl()
+	{
+		contractor = ContractorContainer.getInstance();
+	}
+	
+	public void createContractor(int ID, String name)
+	{
+		contractor.addContractor(ID, name);
+	}
+	
+	public ArrayList<Contractor> getContractors(String name)
+	{
+		return contractor.getContractors(name);
+	}
+	
+	public Contractor getContractor(int ID)
+	{
+		return contractor.getContractor(ID);
+	}
 
 }
--- Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/OfferControl.java
+++ Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/OfferControl.java
@@ -1,5 +1,17 @@
 package ucn.dmf83.sem1project.group4.ControlLayer;
 
+import ucn.dmf83.sem1project.group4.DomainLayer.*;
+
 public class OfferControl {
+	
+	private Offer offer;
+	private OfferContainer offerContainer;
+	
+	public OfferControl()
+	{
+		offerContainer = OfferContainer.getInstance();
+	}
+	
+	
 
 }
