7df0fac9b836a048b9cf171afa04f7a20b1547b5
Jozsef Kiraly <fonix232@gmail.com>
2013-02-03 15:53:55 +0100

    Added save-load feature
    Chris, please update all Controls accordingly!
    
    Signed-off-by: Jozsef Kiraly <fonix232@gmail.com>

--- Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/ContractorControl.java
+++ Software Construction/ucn.dmf83.sem1project.group4/src/ucn/dmf83/sem1project/group4/ControlLayer/ContractorControl.java
@@ -1,5 +1,10 @@
 package ucn.dmf83.sem1project.group4.ControlLayer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
 import ucn.dmf83.sem1project.group4.DomainLayer.*;
@@ -30,11 +35,29 @@ public class ContractorControl {
 	
 	public void readFile()
 	{
+		try {
+		FileInputStream saveFile = new FileInputStream("contractor.dat");
+		ObjectInputStream restore = new ObjectInputStream(saveFile);
+		contractor = (ContractorContainer) restore.readObject();
+		restore.close();
+		saveFile.close();
 		
+		} catch(Exception e) {}
 	}
 	
 	public void saveFile()
 	{
+		try {
+			FileOutputStream saveFile = new FileOutputStream("contractor.dat");
+			ObjectOutputStream save = new ObjectOutputStream(saveFile);
+			save.writeObject(contractor);
+			save.close();
+			saveFile.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
